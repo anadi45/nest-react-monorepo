18ef8a1e2ff6ccd013757b5839b006c1a1fe4ddf
anadi45 <ranjeetbaraik15@gmail.com>
2025-09-10 02:53:03 +0530

    feat(client): add project configuration and update Vite setup
    - Created a new project configuration file `project.json` for the client application, defining build, serve, preview, test, and lint targets.
    - Updated `vite.config.ts` to use dynamic import for the React plugin and added test configuration for Vitest.
    - Introduced a new test setup file `test-setup.ts` to include custom Jest matchers from Testing Library.

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,29 +1,37 @@
 /// <reference types='vitest' />
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 
-export default defineConfig(() => ({
-  root: __dirname,
-  cacheDir: '../node_modules/.vite/client',
-  server:{
-    port: 5173,
-    host: 'localhost',
-  },
-  preview:{
-    port: 5173,
-    host: 'localhost',
-  },
-  plugins: [react()],
-  // Uncomment this if you are using workers.
-  // worker: {
-  //  plugins: [ nxViteTsPaths() ],
-  // },
-  build: {
-    outDir: './dist',
-    emptyOutDir: true,
-    reportCompressedSize: true,
-    commonjsOptions: {
-      transformMixedEsModules: true,
+export default defineConfig(async () => {
+  const react = await import('@vitejs/plugin-react');
+  
+  return {
+    root: __dirname,
+    cacheDir: '../node_modules/.vite/client',
+    server:{
+      port: 5173,
+      host: 'localhost',
     },
-  },
-}));
+    preview:{
+      port: 5173,
+      host: 'localhost',
+    },
+    plugins: [react.default()],
+    // Uncomment this if you are using workers.
+    // worker: {
+    //  plugins: [ nxViteTsPaths() ],
+    // },
+    build: {
+      outDir: './dist',
+      emptyOutDir: true,
+      reportCompressedSize: true,
+      commonjsOptions: {
+        transformMixedEsModules: true,
+      },
+    },
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: ['src/test-setup.ts'],
+    },
+  };
+});
